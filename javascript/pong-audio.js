bc840e3200a0adb1b67267f63b563bfcc4b6a32d
gesuinacampos <gesuinacampos@me.com>
2025-01-08 09:40:05 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -46,7 +46,7 @@ export var soundArray = []; //list of sounds loaded
 export var wallSound = new soundFile("wallnoise.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
-export var paddleSound = new soundFile("paddlenoise.mp3");
+export var paddleSound = new soundFile("pongnoise.mp3");
 soundArray.push(paddleSound);
 
 export var scoreSound = new soundFile("silence.mp3");
